f5b7dcc13e4e8c6a6a956e812357e34794c2ee7a
rxssett11 <rxssette11@gmail.com>
2025-04-18 21:57:57 -0600

    configSession

--- includes/configSession.php
+++ includes/configSession.php
@@ -4,7 +4,6 @@ session_start();
 $usuario  = $_SESSION['usuario'];
 if (!isset($_SESSION['usuario'])) {
     echo "<script>alert('Primero debes iniciar sesión y Validar tus credenciales..');</script>";
-
     echo "<script>location.assign('../index.php');</script>";
     exit();
 }
